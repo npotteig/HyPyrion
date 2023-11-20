7678fc5260aee61635fb316b612a358ab277eeeb
npotteig <npottei1@umbc.edu>
2023-11-20 03:30:52 -0600

    Removed extra test file

--- test.py
+++
@@ -1,8 +0,0 @@
-from hyper.transforms import PolarTransform
-import numpy as np
-
-ori = PolarTransform(0, 0, 0)
-my_trans = PolarTransform(2*np.pi/5, 1.255, np.pi)
-ori.step(my_trans.s)
-# print(my_trans.to_string())
-# print(np.arctan2(0, -1))
